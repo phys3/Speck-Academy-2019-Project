0ed317198e99638158a3247ba4ff1d64bdc51bce
Damjan Coric <damjan.coric@mail.com>
2019-03-21 19:33:58 +0100

    Added Array

---
+++ halls.js
@@ -0,0 +1,47 @@
+var dvorane = [
+    {
+        id: 1,
+        name: "dvo1",
+        reservation: {
+            isReserved: true,
+            reservedFrom: ["Thu Mar 21 2019 17:00:00 GMT+0100 (Central European Standard Time)"],
+            reservedUntill: ["Thu Mar 22 2019 17:00:00 GMT+0100 (Central European Standard Time)"],
+        }
+    },
+    {
+        id: 2,
+        name: "dvo2",
+        reservation: {
+            isReserved: true,
+            reservedFrom: ["Thu Mar 21 2019 17:00:00 GMT+0100 (Central European Standard Time)"],
+            reservedUntill: ["Thu Mar 23 2019 14:00:00 GMT+0100 (Central European Standard Time)"],
+        }
+    },
+    {
+        id: 3,
+        name: "dvo3",
+        reservation: {
+            isReserved: false,
+            reservedFrom: null,
+            reservedUntill: null,
+        }
+    },
+    {
+        id: 4,
+        name: "dvo4",
+        reservation: {
+            isReserved: false,
+            reservedFrom: null,
+            reservedUntill: null,
+        }
+    },
+    {
+        id: 5,
+        name: "dvo5",
+        reservation: {
+            isReserved: false,
+            reservedFrom: null,
+            reservedUntill: null,
+        }
+    },
+];
