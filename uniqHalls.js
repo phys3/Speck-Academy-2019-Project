114d4de2e764a47579fd339655f3cfab3a7f2e7d
phys3 <damjan.coric@mail.com>
2019-03-25 19:46:01 +0100

    Npm run start-uniqHalls added in package.json

--- uniqHalls.js
+++ uniqHalls.js
@@ -3,5 +3,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(dvorane[0].name);
+    res.end(dvorane[1].name);
 }).listen(8080);;
